dba40723ab1af0a092a741274f9f8f45be150a2d
Alif Maulida Musyarofah <alifmaulida7887@gmail.com>
2025-08-19 20:29:37 +0700

    Tambah View pos-bandara-jember, routes web, dan inventaris-alat.create

--- resources/views/inventaris-alat/index.blade.php
+++ resources/views/inventaris-alat/index.blade.php
@@ -195,7 +195,7 @@
             </div>
 
             <div class="col-12 col-sm-6 col-xl-3 mb-4">
-                <a href="" class="card-link">
+                <a href="/cek-alat/pos-bandara-bwi" class="card-link">
                     <div class="card border-0 shadow text-center hover-card">
                         <div class="card-body d-flex flex-column align-items-center justify-content-center">
                             <!-- Ikon -->
@@ -226,7 +226,7 @@
             </div>
 
             <div class="col-12 col-sm-6 col-xl-3 mb-4">
-                <a href="" class="card-link">
+                <a href="/cek-alat/ketapang" class="card-link">
                     <div class="card border-0 shadow text-center hover-card">
                     <div class="card-body d-flex flex-column align-items-center justify-content-center">
                         <!-- Ikon -->
@@ -249,7 +249,7 @@
             </div>
 
             <div class="col-12 col-sm-6 col-xl-3 mb-4">
-                <a href="" class="card-link">
+                <a href="/cek-alat/pos-bandara-jember" class="card-link">
                     <div class="card border-0 shadow text-center hover-card">
                     <div class="card-body d-flex flex-column align-items-center justify-content-center">
                         <!-- Ikon -->
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\KantorBmkgController;
+use App\Http\Controllers\PosBandaraBwiController;
+use App\Http\Controllers\PosBandaraJemberController;
+use App\Http\Controllers\KetapangController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +27,6 @@ Route::get('/cek-alat', function () {
 
 
 Route::get('cek-alat/kantor-bmkg', [KantorBmkgController::class, 'create'])->name('kantor-bmkg.create');
+Route::get('cek-alat/pos-bandara-bwi', [PosBandaraBwiController::class, 'create'])->name('pos-bandara-bwi.create');
+Route::get('cek-alat/pos-bandara-jember', [PosBandaraJemberController::class, 'create'])->name('pos-bandara-jember.create');
+Route::get('cek-alat/ketapang', [KetapangController::class, 'create'])->name('ketapang.create');
