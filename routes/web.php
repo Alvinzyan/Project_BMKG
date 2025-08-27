d18f042daf4739d9960c972675bc2cdf8a6efd7a
cnndl-22 <cindilestari22@gmail.com>
2025-08-27 22:39:12 +0700

    Tambah route untuk Data Alat

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::get('cek-alat/kantor-bmkg', [KantorBmkgController::class, 'create'])->nam
 Route::get('cek-alat/pos-bandara-bwi', [PosBandaraBwiController::class, 'create'])->name('pos-bandara-bwi.create');
 Route::get('cek-alat/pos-bandara-jember', [PosBandaraJemberController::class, 'create'])->name('pos-bandara-jember.create');
 Route::get('cek-alat/ketapang', [KetapangController::class, 'create'])->name('ketapang.create');
+
+Route::get('/data-alat', function () {
+    return view('data-alat.index');
+});
