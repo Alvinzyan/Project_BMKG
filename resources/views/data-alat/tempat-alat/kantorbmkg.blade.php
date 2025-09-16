25b07bccaaa2fb49d934a823dcfdb35cad1168ec
Alif Maulida Musyarofah <alifmaulida7887@gmail.com>
2025-09-16 16:52:44 +0700

    menambahkan sedikit kode di tempat alat kantorbmkg.blade.php

--- resources/views/data-alat/tempat-alat/kantorbmkg.blade.php
+++ resources/views/data-alat/tempat-alat/kantorbmkg.blade.php
@@ -189,7 +189,7 @@
         <div class="row">  
                 <div class="row row-cols-1 row-cols-md-2 g-3 justify-content-center">
                     <div class="col-6">
-                        <a href="">
+                        <a href="../tambah-data-alat/index">
                             <div class="card-two border-0 shadow hover-card">
                                 <div class="card-body d-flex align-items-center">
                                     <!-- ICON -->
