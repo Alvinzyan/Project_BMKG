7689e71879d09bdaee185b6f4158edc4d99b0155
Alvindra Ibnu <151527316+Alvinzyan@users.noreply.github.com>
2025-10-08 07:56:08 +0700

    mengubah migrasi tabel users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->text('nama_lengkap');
-            $table->string('username')->unique();
+            $table->string('email')->unique();
             $table->string('password');
+            $table->text('nama_lengkap')->nullable();
+            $table->string('nip')->unique()->nullable();
+            $table->text('jabatan')->nullable();
+            $table->enum('jenis_kelamin', ['laki laki', 'perempuan'])->nullable();
+            $table->enum('peran', ['admin', 'teknisi']);
             $table->rememberToken();
             $table->timestamps();
         });
