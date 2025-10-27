fb7bd9abf22c86df7da94cc7eb325a166e51cc51
Alvindra Ibnu <151527316+Alvinzyan@users.noreply.github.com>
2025-10-27 13:21:16 +0700

    menambahkan admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'email' => 'admin@gmail.com',
+            'password' => Crypt::encryptString('AdminBmkg321'),
+            'nama_lengkap' => 'Administrator',
+            'peran' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
