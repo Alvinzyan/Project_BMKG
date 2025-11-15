8007625549206759b97242f9903dde22ce383827
Alif Maulida Musyarofah <alifmaulida7887@gmail.com>
2025-11-15 10:26:58 +0700

    mengubah sedikit kode untuk store dan update pengecekan alat

--- app/Http/Controllers/PosBandaraJemberController.php
+++ app/Http/Controllers/PosBandaraJemberController.php
@@ -59,8 +59,8 @@ class PosBandaraJemberController extends Controller
             $fotoLampiranPath = null;
             if ($request->hasFile("foto_lampiran.$alatId")) {
                 $file = $request->file("foto_lampiran.$alatId");
-                $namaFile = time() . '_' . $file->getClientOriginalName();
-                $fotoLampiranPath = $file->storeAs('public/foto_pengecekan', $namaFile); 
+                $namaFile = time() . '.' . $file->getClientOriginalExtension();
+                $fotoLampiranPath = $file->storeAs('public/foto_pengecekan', $namaFile);
             }
             
             Pengecekan::create([
@@ -83,7 +83,7 @@ class PosBandaraJemberController extends Controller
             }
         }
 
-        return redirect()->route('pos-bandara-jmbr.create')->with('success', 'Data pengecekan alat berhasil disimpan.');
+        return redirect()->route('kantor-bmkg.index')->with('success', 'Data pengecekan alat berhasil disimpan.');
     }
 
     /**
@@ -126,17 +126,27 @@ class PosBandaraJemberController extends Controller
 
             if ($request->hasFile("foto_lampiran.$alatId")) {
                 $file = $request->file("foto_lampiran.$alatId");
-                $namaFile = time() . '_' . $file->getClientOriginalName();
+                $namaFile = time() . '.' . $file->getClientOriginalExtension();
                 $fotoLampiranPath = $file->storeAs('public/foto_pengecekan', $namaFile);
             }
             
-            Pengecekan::create([
-                'id_user' => $userId,
-                'id_alat' => $alatId,
-                'kondisi' => $kondisiArray,
-                'kalibrasi_terakhir' => $request->kalibrasi[$alatId] ?? null,
-                'foto_lampiran' => $fotoLampiranPath,
-            ]);
+            $pengecekan = Pengecekan::where('id_alat', $alatId)->latest()->first();
+            if ($pengecekan) {
+                $pengecekan->update([
+                    'id_user' => $userId,
+                    'kondisi' => $kondisiArray,
+                    'kalibrasi_terakhir' => $request->kalibrasi[$alatId] ?? null,
+                    'foto_lampiran' => $fotoLampiranPath ? str_replace('public/','',$fotoLampiranPath) : $pengecekan->foto_lampiran,
+                ]);
+            } else {
+                Pengecekan::create([
+                    'id_user' => $userId,
+                    'id_alat' => $alatId,
+                    'kondisi' => $kondisiArray,
+                    'kalibrasi_terakhir' => $request->kalibrasi[$alatId] ?? null,
+                    'foto_lampiran' => $fotoLampiranPath ? str_replace('public/','',$fotoLampiranPath) : null,
+                ]);
+            }
         }
 
         if ($request->has('catatan')) {
@@ -156,7 +166,7 @@ class PosBandaraJemberController extends Controller
             }
         }
 
-        return redirect()->route('pos-bandara-jmbr.edit')
+        return redirect()->route('kantor-bmkg.index')
             ->with('success', 'Data pengecekan alat dan catatan berhasil diperbarui.');
     }
 
